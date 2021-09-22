4834a5fbd6140a2f19a7597edb3f09bc8630a944
wnstjr <wnstjr8717@gmail.com>
2021-09-22 22:28:10 +0900

    faster connection

--- app.py
+++ app.py
@@ -14,27 +14,28 @@ timezone_kst = datetime.timezone(datetime.timedelta(hours=9))
 
 # 클라이언트 접속이 되면 호출된다.
 async def accept(websocket, path):
-    data = await websocket.recv()
-    data = json.loads(data)
-    print("receive : " + str(data))
+    while True:
+        data = await websocket.recv()
+        data = json.loads(data)
+        print("receive : " + str(data))
 
-    # 프로젝트 생성
-    if data['create_project']:
-        k, v = map(lambda x: x[0], zip(data['create_project']))
-        # TODO k값 중복일때 예외처리
-        PM.projects[k] = {'metadata': [v, pm.getfilename(v),
-                                       datetime.datetime.now(timezone_kst).strftime('%Y.%m.%d %H:%M')],
-                          'work': {}}
+        # 프로젝트 생성
+        if data['create_project']:
+            k, v = map(lambda x: x[0], zip(data['create_project']))
+            # TODO k값 중복일때 예외처리
+            PM.projects[k] = {'metadata': [v, pm.getfilename(v),
+                                           datetime.datetime.now(timezone_kst).strftime('%Y.%m.%d %H:%M')],
+                              'work': {}}
 
-    pid = data['get_project_work']
-    # 프로젝트 리스트 반환
-    if not pid:
-        ret = list(PM.projects.keys())
-    # 선택한 프로젝트 작업 반환
-    else:
-        ret = PM.projects[pid]
+        pid = data['get_project_work']
+        # 프로젝트 리스트 반환
+        if not pid:
+            ret = list(PM.projects.keys())
+        # 선택한 프로젝트 작업 반환
+        else:
+            ret = PM.projects[pid]
 
-    await websocket.send(json.dumps(ret))
+        await websocket.send(json.dumps(ret))
 
 
 if __name__ == "__main__":
