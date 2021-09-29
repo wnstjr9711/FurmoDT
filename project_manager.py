f04e67cc41fffbe6f6bff2d70cd792d84eb432ce
wnstjr <wnstjr8717@gmail.com>
2021-09-29 10:44:29 +0900

    작업 update

--- project_manager.py
+++ project_manager.py
@@ -12,21 +12,26 @@ class ProjectManager:
 
     def save(self, data):
         """
-        :param data: {1: create_project, 2: delete_project, 3: add_language}
+        :param data: {1: create_project, 2: delete_project, 3: add_language, 4: update_work}
         """
         if '0' in data:
             pass
         elif '1' in data:
             # TODO key 값 중복일때 예외처리
             key, url, video = data['1']
-            self.work[key] = pd.DataFrame([[i for i in range(4)] for j in range(10)],
-                                          columns=['번호', 'TC_IN', 'TC_OUT', '원어'])
+            header = ['번호', 'TC_IN', 'TC_OUT', '원어']
+            self.work[key] = pd.DataFrame([['' for i in range(len(header))] for j in range(2000)],
+                                          columns=header)
             self.projects[key] = {'metadata': {'url': url, 'video': video, 'date': datetime.now(KST).strftime('%Y.%m.%d %H:%M'), 'key': key},
                                   'work': self.work[key]
                                   }
         elif '3' in data:
             key, language = data['3']
-            self.work[key][language] = np.nan
+            self.work[key][language] = ''
+
+        elif '4' in data:
+            key, row, column, text = data['4']
+            self.work[key].iloc[row, column] = text
 
     def get_project_json(self, pid):
         project = dict(self.projects[pid])
